8ac7487a61ee6e0a2657074cb09f722c027e0e1d
Glenn Lopez Diaz <ostenfor@gmail.com>
2026-05-03 18:01:58 -0400

    refactor: move report file utilities to module

--- src/utils/fileUtils.js
+++ src/utils/fileUtils.js
@@ -26,6 +26,59 @@ const openHtmlFile = (filePath) => {
   });
 };
 
+const getReportDateForFileName = () => {
+  return new Intl.DateTimeFormat('en-CA', {
+    timeZone: 'America/Santo_Domingo',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+  }).format(new Date());
+};
+
+const getTodayStringRD = () => {
+  return new Intl.DateTimeFormat('en-US', {
+    timeZone: 'America/Santo_Domingo',
+    month: '2-digit',
+    day: '2-digit',
+    year: 'numeric'
+  }).format(new Date());
+};
+
+const getReportsFolderPath = () => {
+  const reportsFolder = path.join(process.cwd(), 'reporte');
+
+  if (!fs.existsSync(reportsFolder)) {
+    fs.mkdirSync(reportsFolder, { recursive: true });
+  }
+
+  return reportsFolder;
+};
+
+const getUniqueReportFilePath = (folderPath, baseName, reportDate) => {
+  const baseFileName = `${baseName}-${reportDate}`;
+  let filePath = path.join(folderPath, `${baseFileName}.html`);
+
+  if (!fs.existsSync(filePath)) {
+    return filePath;
+  }
+
+  let counter = 1;
+
+  while (true) {
+    filePath = path.join(folderPath, `${baseFileName}.${counter}.html`);
+
+    if (!fs.existsSync(filePath)) {
+      return filePath;
+    }
+
+    counter++;
+  }
+};
+
 module.exports = {
-  openHtmlFile
+  openHtmlFile,
+  getReportDateForFileName,
+  getTodayStringRD,
+  getReportsFolderPath,
+  getUniqueReportFilePath
 };
--- testa.js
+++ testa.js
@@ -24,7 +24,11 @@ const {
 } = require('./src/utils/navigationUtils');
 
 const {
-  openHtmlFile
+  openHtmlFile,
+  getReportDateForFileName,
+  getTodayStringRD,
+  getReportsFolderPath,
+  getUniqueReportFilePath
 } = require('./src/utils/fileUtils');
 // ==================================================
 // END MODULE 01 - BOOT
@@ -265,62 +269,6 @@ const renderNoteLabels = (publisher) => {
   };
   // ==================================================
   // END MODULE 07 - CONSOLE
-  // ==================================================
-
-
-  // ==================================================
-  // MODULE 08 - FILES
-  // ==================================================
-  const getReportDateForFileName = () => {
-    return new Intl.DateTimeFormat('en-CA', {
-      timeZone: 'America/Santo_Domingo',
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit'
-    }).format(new Date());
-  };
-
-  const getTodayStringRD = () => {
-    return new Intl.DateTimeFormat('en-US', {
-      timeZone: 'America/Santo_Domingo',
-      month: '2-digit',
-      day: '2-digit',
-      year: 'numeric'
-    }).format(new Date());
-  };
-
-  const getReportsFolderPath = () => {
-    const reportsFolder = path.join(__dirname, 'reporte');
-
-    if (!fs.existsSync(reportsFolder)) {
-      fs.mkdirSync(reportsFolder, { recursive: true });
-    }
-
-    return reportsFolder;
-  };
-
-  const getUniqueReportFilePath = (folderPath, baseName, reportDate) => {
-    const baseFileName = `${baseName}-${reportDate}`;
-    let filePath = path.join(folderPath, `${baseFileName}.html`);
-
-    if (!fs.existsSync(filePath)) {
-      return filePath;
-    }
-
-    let counter = 1;
-
-    while (true) {
-      filePath = path.join(folderPath, `${baseFileName}.${counter}.html`);
-
-      if (!fs.existsSync(filePath)) {
-        return filePath;
-      }
-
-      counter++;
-    }
-  };
-  // ==================================================
-  // END MODULE 08 - FILES
   // ==================================================
 
 
